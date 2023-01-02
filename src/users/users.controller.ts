7688848a60373e00128455c398cd5a49e4aa5567
dyWe2216 <dnleodud@gmail.com>
2023-01-02 17:24:14 +0900

    add: signIn && signUp method added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -8,30 +18,38 @@ import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
-    }
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
 
-    @Get(':id')
-    findUser(@Param('id') id: string) {
-        return this.usersService.findOne(+id);
-    }
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
 
-    @Get()
-    findAllUsers(@Query('email') email: string) {
-        return this.usersService.find(email);
-    }
+  @Get(':id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
 
-    @Delete(':id')
-    removeUser(@Param('id') id: string) {
-        return this.usersService.remove(+id);
-    }
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
 
-    @Patch(':id')
-    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-        return this.usersService.update(+id, body);
-    }
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(+id, body);
+  }
 }
