94e5b85acc953f5988f528b26ee9fd628a7b1ffd
dyWe2216 <dnleodud@gmail.com>
2023-01-03 13:37:36 +0900

    add: controller test code added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,10 @@ import {
   Post,
   Query,
   Session,
-  // UseGuards,
+  UseGuards,
 } from '@nestjs/common';
-// import { AuthGuard } from 'src/guards/auth.guard';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthGuard } from '../guards/auth.guard';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -28,7 +28,7 @@ export class UsersController {
   ) {}
 
   @Get('/whoami')
-  // @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user: string) {
     return user;
   }
